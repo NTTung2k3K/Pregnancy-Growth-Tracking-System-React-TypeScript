12e8054b2a7049bd8d730a1b638d39f3bb31f5c0
Thang Lam <thanglam271003@gmail.com>
2025-01-19 01:23:17 +0700

    login by google

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import{ GoogleOAuthProvider } from '@react-oauth/google';
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId='349014088899-9tap25m4qidc2s8n8b4t8o82i737k9ac.apps.googleusercontent.com'>
+        <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
